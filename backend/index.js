ca1b42daac819fe0406e6a59f7fe4cffaed56bda
mzikojioshvili <mzia.jioshvili@gmail.com>
2026-07-09 17:00:16 +0400

    added dynamic port

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,8 @@ app.use("/api/auth", authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/quizzes', quizRoutes);
 
-app.listen(4000, () => {
-    console.log('Server is running on port http://localhost:4000');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
 })
